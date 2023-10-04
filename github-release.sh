85910f8462dff368b8558a1f2c432195aa863e3d
Christian Beikov <christian.beikov@gmail.com>
2023-10-05 01:16:24 +0200

    HHH-17239 Automate maintenance releases

--- github-release.sh
+++ github-release.sh
@@ -125,6 +125,7 @@ See also the following resources related to supported APIs:
 
 Visit the [website](https://hibernate.org/community/) for details on getting in touch with us."""
 
+set -x
 response=$(exec_or_dry_run curl -L -s -w "\n%{http_code}" -X POST \
 	-H 'Accept: application/vnd.github+json' \
 	-H 'X-GitHub-Api-Version: 2022-11-28' \
