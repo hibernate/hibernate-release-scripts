4a5c232a5ef21db0b6e92c8979c6b928ce73f8e5
Christian Beikov <christian.beikov@gmail.com>
2023-10-05 01:21:05 +0200

    HHH-17239 Automate maintenance releases

--- github-release.sh
+++ github-release.sh
@@ -125,12 +125,11 @@ See also the following resources related to supported APIs:
 
 Visit the [website](https://hibernate.org/community/) for details on getting in touch with us."""
 
-set -x
 response=$(exec_or_dry_run curl -L -s -w "\n%{http_code}" -X POST \
 	-H 'Accept: application/vnd.github+json' \
 	-H 'X-GitHub-Api-Version: 2022-11-28' \
 	-H "Authorization: Bearer \$GITHUB_API_TOKEN" \
-	-d ''\''{"tag_name":"'${RELEASE_VERSION}'","name":"'${releaseName}'","make_latest":"legacy","body":"'${releaseBody}'"}'\''' \
+	-d '"{\"tag_name\":\"'${RELEASE_VERSION}'\",\"name\":\"'${releaseName}'\",\"make_latest\":\"legacy\",\"body\":\"'${releaseBody}'\"}"' \
 	'https://api.github.com/repos/hibernate/hibernate-orm/releases')
 
 if [ "$PUSH_CHANGES" == true ]; then
