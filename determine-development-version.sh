945e92d012cfa47f7d305153b37a40f88175fb08
Yoann Rodière <yoann@hibernate.org>
2024-06-13 12:33:17 +0200

    determine-development-version: Handle Alpha/Beta/CR versions

--- determine-development-version.sh
+++ determine-development-version.sh
@@ -11,5 +11,10 @@ if [ -z "$RELEASE_VERSION" ]; then
 fi
 
 RELEASE_VERSION_FAMILY=$(echo "$RELEASE_VERSION" | sed -E 's/^([0-9]+\.[0-9]+).*/\1/')
-NEXT_MINOR=$(echo "$RELEASE_VERSION" | sed -E 's/^[0-9]+\.[0-9]+\.([0-9]+).*/\1/' | awk '{print $0+1}')
-echo "${RELEASE_VERSION_FAMILY}.${NEXT_MINOR}-SNAPSHOT"
+if echo "$RELEASE_VERSION" | grep -q -P '^[0-9]+\.[0-9]+\.0\.(?!Final$).*'; then
+	# Alpha/Beta/CR release: next snapshot keeps the same version numbers
+	echo "${RELEASE_VERSION_FAMILY}.0-SNAPSHOT"
+else
+	NEXT_MINOR=$(echo "$RELEASE_VERSION" | sed -E 's/^[0-9]+\.[0-9]+\.([0-9]+).*/\1/' | awk '{print $0+1}')
+	echo "${RELEASE_VERSION_FAMILY}.${NEXT_MINOR}-SNAPSHOT"
+fi
