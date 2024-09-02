1be544bb9e95a0c20fe1ae00df045aebcc6f83fb
marko-bekhta <marko.prykladna@gmail.com>
2024-09-02 13:22:09 +0200

    Revert "Add branches for infra projects"
    This reverts commit c3ed39689db2213acfa1e54d80772530004a9d24.
    This reverts commit 6c0560eac5a28f7897ea0477406036c2c7ac49a7.
    This reverts commit 1d4f4287a2d748c7b32668fd107b9f7e0a532f1e.

--- publish.sh
+++ publish.sh
@@ -99,8 +99,6 @@ elif [ "$PROJECT" == "reactive" ]; then
 	fi
 	# Publish the artifact to OSSRH
 	exec_or_dry_run ./gradlew publishToSonatype closeAndReleaseSonatypeStagingRepository -PsontaypeOssrhUser=$OSSRH_USER -PsonatypeOssrgPassword=$OSSRH_PASSWORD
-elif [[ "$PROJECT" == "infra-theme" || "$PROJECT" == "infra-extensions" ]]; then
-  exec_or_dry_run ./mvnw release:perform -DperformRelease=true -Dmaven.resolver.transport=wagon
 else
 	bash -xe "$SCRIPTS_DIR/deploy.sh" "$PROJECT"
 
