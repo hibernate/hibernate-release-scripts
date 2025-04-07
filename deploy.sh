e78775f686738e0a68261b84713253bd7efaa997
marko-bekhta <marko.prykladna@gmail.com>
2025-04-07 15:56:36 +0200

    Add JReleaser execution branch (for maven projects)
    Signed-off-by: marko-bekhta <marko.prykladna@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -33,6 +33,8 @@ else
 
 	source "$SCRIPTS_DIR/mvn-setup.sh"
 
+	# if there's no JReleaser file available the deploy command will use the nexus-staging plugin and deploy to nexus itself.
+	# otherwise the deploy plugin pushes the artifacts to a local directory that jreleaser will work with:
 	./mvnw clean deploy \
 		-Pdocbook,documentation-pdf,dist,perf,relocation,release \
 		-DperformRelease=true \
@@ -43,6 +45,13 @@ else
 		-Dgradle.cache.remote.enabled=false -Dgradle.cache.local.enabled=false \
 		-Ddevelocity.cache.remote.enabled=false -Ddevelocity.cache.local.enabled=false \
 	 	$ADDITIONAL_OPTIONS
+
+	if [ -f "./jreleaser.yml" ]; then
+		# JReleaser-based build
+		export JRELEASER_GPG_HOMEDIR="$RELEASE_GPG_HOMEDIR"
+		curl https://jreleaser.org/setup.sh -sSfL | sh
+		jreleaser full-release -Djreleaser.project.version="$RELEASE_VERSION"
+	fi
 fi
 
 popd
