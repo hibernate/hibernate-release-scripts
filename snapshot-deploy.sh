f02c265c0a8896851908130fdbbbed5c3c742168
marko-bekhta <marko.prykladna@gmail.com>
2025-04-10 12:30:49 +0200

    Add snapshot publishing script
    Signed-off-by: marko-bekhta <marko.prykladna@gmail.com>

---
+++ snapshot-deploy.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env -S bash -e
+
+WORKSPACE="${WORKSPACE:-'.'}"
+SCRIPTS_DIR="$(readlink -f ${BASH_SOURCE[0]} | xargs dirname)"
+
+PROJECT="$1"
+VERSION="$2"
+
+if [ -z "$PROJECT" ]; then
+	echo "ERROR: Project not supplied"
+	exit 1
+fi
+
+if [ -z "$VERSION" ]; then
+	echo "ERROR: Version not supplied"
+	exit 1
+fi
+
+if ! [[ $VERSION =~ ^.+-SNAPSHOT$ ]]; then
+	echo "ERROR: Supplied Version (${VERSION}) is not a SNAPSHOT"
+	exit 1
+fi
+
+source "$SCRIPTS_DIR/jreleaser-setup.sh"
+
+if [ "$PROJECT" == "search" ] || [ "$PROJECT" == "validator" ] || [ "$PROJECT" == "tools" ] || [[ $PROJECT =~ ^infra-.+ ]]; then
+  ./mvnw -Pci-build -DskipTests clean deploy
+elif [ "$PROJECT" == "orm" ] || [ "$PROJECT" == "reactive" ] || [ "$PROJECT" == "models" ]; then
+  ./gradlew clean publish -x test --no-scan --no-daemon --no-build-cache --stacktrace
+else
+  echo "ERROR: Unknown project name $PROJECT"
+  usage
+  exit 1
+fi
+
+# Execute a JReleaser command such as 'full-release'
+./jreleaser/bin/jreleaser full-release -Djreleaser.project.version="$VERSION"
