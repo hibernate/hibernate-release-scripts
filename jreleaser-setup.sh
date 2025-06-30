98644ae6fee7c12e198944e2829d8d7ec5dedf3b
marko-bekhta <marko.prykladna@gmail.com>
2025-06-30 23:46:00 +0200

    Update JReleaser to 1.19.0
    - and add hash verification of the downloaded zip
    
    Signed-off-by: marko-bekhta <marko.prykladna@gmail.com>

--- jreleaser-setup.sh
+++ jreleaser-setup.sh
@@ -12,11 +12,23 @@ if [ -f "${SCRIPTS_DIR}/jreleaser/bin/jreleaser" ]; then
   return
 fi
 
+EXPECTED_HASH="5a20df93b51654f6a06984a587e4c3595f5746b95f202b571d707315a2191efe"
+JRELEASER_VERSION="1.19.0"
+
 echo "About to install JReleaser."
-wget https://github.com/jreleaser/jreleaser/releases/download/v1.18.0/jreleaser-1.18.0.zip -qO jreleaser.zip
+wget "https://github.com/jreleaser/jreleaser/releases/download/v$JRELEASER_VERSION/jreleaser-$JRELEASER_VERSION.zip" -qO jreleaser.zip
+
+
+DOWNLOADED_HASH=$(sha256sum jreleaser.zip | awk '{print $1}')
+if [ "$DOWNLOADED_HASH" == "$EXPECTED_HASH" ]; then
+    echo "Successfully verified the file hash"
+else
+    echo "Error: Failed the hash verification. Expected: $EXPECTED_HASH but got $DOWNLOADED_HASH instead"
+    exit 1
+fi
 unzip -qq jreleaser.zip
-mv jreleaser-1.18.0/* $SCRIPTS_DIR/jreleaser
-rm -r jreleaser-1.18.0
+mv "jreleaser-$JRELEASER_VERSION"/* $SCRIPTS_DIR/jreleaser
+rm -r "jreleaser-$JRELEASER_VERSION"
 rm jreleaser.zip
 
 $SCRIPTS_DIR/jreleaser/bin/jreleaser --version
